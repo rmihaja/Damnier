5381ce01a4f03ce04dbfe6e21401ce1d5020b31c
Rmihaja <50056520+Rmihaja@users.noreply.github.com>
2021-03-05 23:05:52 +0100

    ✨ Add board square generation
    Modify board style
    Modify Tkinter root properties
    Improve code structure/code design

--- app.py
+++ app.py
@@ -2,27 +2,56 @@ from tkinter import *
 
 ############### MODELE ###############
 
-# *
+# * variable init
+
+board = []
+boardSize = 8  # ? american chekers has 8 rows and 8 columns
+
+
+# * functions init
+
+def createBoard(size):
+    board = []
+    # filling the board with oponent pieces
+    for row in range(0, size):
+        boardRow = []
+
+        # check if boardRow belongs to players
+        if (row < (size / 2) - 1):
+            piece = 'O'  # ? O = opponent
+        elif (row > (size / 2)):
+            piece = 'P'  # ? P = player
+        else:
+            piece = 'E'  # ? E = empty : available square not occupied
+
+        # filling the row
+        for column in range(0, size):
+            if ((row % 2 == 0 and column % 2 != 0) or (row % 2 != 0 and column % 2 == 0)):
+                boardRow.append(piece)
+            else:
+                boardRow.append('')
+        board.append(boardRow)
+
+    return board
+
 
 ############### VIEW ###############
 
 # * variable init
-BOARD_SIZE = 8  # ? american chekers has 8 rows and 8 columns
 BOARD_LENGTH = 800
-BOARD_COLOR = '#c1c1c1'
-SQUARE_LENGTH = BOARD_LENGTH / BOARD_SIZE
+BOARD_COLOR = '#010101'
+SQUARE_LENGTH = BOARD_LENGTH / boardSize
 SQUARE_COLOR = '#000'
 PIECE_RADIUS = (0.95 * SQUARE_LENGTH) / 2
-PLAYER_COLOR = "#DB380D"
+PLAYER_COLOR = "#FD0002"
 OPPONENT_COLOR = BOARD_COLOR
 
 # Tkinter init
 root = Tk()
-cnv = Canvas(root, width=BOARD_LENGTH,
-             height=BOARD_LENGTH, background=BOARD_COLOR)
+root.title('Damnier')
+root.geometry(str(BOARD_LENGTH) + 'x' + str(BOARD_LENGTH))
 
 # render gui
-cnv.pack()
 
 # run tkinter
-root.mainloop()
+# root.mainloop()
