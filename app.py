9121c850fcae0ead7e914107a1d692943833fe99
Rmihaja <50056520+Rmihaja@users.noreply.github.com>
2021-03-05 17:51:03 +0100

    🚀 Add board game layout
    Add board properties
    Add board view generation

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
 from tkinter import *
+
+############### MODELE ###############
+
+# *
+
+############### VIEW ###############
+
+# * variable init
+BOARD_SIZE = 8  # ? american chekers has 8 rows and 8 columns
+BOARD_LENGTH = 800
+BOARD_COLOR = '#c1c1c1'
+SQUARE_LENGTH = BOARD_LENGTH / BOARD_SIZE
+SQUARE_COLOR = '#000'
+PIECE_RADIUS = (0.95 * SQUARE_LENGTH) / 2
+PLAYER_COLOR = "#DB380D"
+OPPONENT_COLOR = BOARD_COLOR
+
+# Tkinter init
+root = Tk()
+cnv = Canvas(root, width=BOARD_LENGTH,
+             height=BOARD_LENGTH, background=BOARD_COLOR)
+
+# render gui
+cnv.pack()
+
+# run tkinter
+root.mainloop()
