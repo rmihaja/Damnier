e63207fef3c3b908bdfd4a13c6f9ab90dc04f25d
Rmihaja <50056520+Rmihaja@users.noreply.github.com>
2021-03-07 00:03:07 +0100

    🔥 Remove board creation on client side
    Create Board class for server
    🚧 Setup board data exchange

---
+++ server/board.js
@@ -0,0 +1,67 @@
+module.exports = class Board {
+    
+    // * constructor init
+    constructor(size) {
+        this.size = size;
+        this.layout = this.generateBoard(this.size);
+    }
+
+    generateBoard(size) {
+        let layout = [];
+        for (let row = 0; row < size; row++) {
+            let boardRow = [];
+            let piece;
+            // check if boardRow belongs to players
+            if (row < (size / 2) - 1) {
+                piece = 'O';  // ? O = opponent
+            } else if (row > (size / 2)) {
+                piece = 'P';  // ? P = player
+            } else {
+                piece = 'E';  // ? E = empty : available square not occupied
+            }
+        
+            
+            // filling the row
+            for (let column = 0; column < size; column++) {
+                if ((row % 2 == 0 && column % 2 != 0) || (row % 2 != 0 && column % 2 == 0)) {
+                    boardRow.push(piece);
+                }
+                else {
+                    boardRow.push('');
+                }
+            }
+            
+            layout.push(boardRow);
+        }
+        return layout;
+    }
+
+    // return stringified array readable by python
+    getBoardLayout(isBlack) {
+        if(!isBlack)
+        {
+            return JSON.stringify(this.layout);
+        }
+        else {
+            return JSON.stringify(this.getBlackLayout());
+        }
+    }
+
+    // flip the board horizontally for player as opponent
+    getBlackLayout() {
+        let blackLayout = this.layout;
+        for (let i = 0; i < blackLayout.length; i++) {
+            blackLayout[i].reverse(); // ? reverse each row
+        }
+        return blackLayout.reverse() // ? reverse column before return
+    }
+
+    movePiece(piecePosition, emptyPosition) {
+        let xpiece, ypiece = piecePosition
+        let xempty, yempty = emptyPosition
+
+        // interchange case value
+        this.layout[xempty][yempty] = 'P'
+        this.layout[xpiece][ypiece] = 'E'
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
+const Board = require('./board'); 
+
 // setup socket.io
 const io = require('socket.io')(3000)
 
 io.on('connection', socket => {
     console.log('someone got connected');
 
+    // send board data to player
+    // io.emit('board', new)
+
     // listen to custom event sent by socket
 })
+
+// board data
+
+let board = new Board(8);
+console.log(board.getBoardLayout(true));
+console.log(Board)
